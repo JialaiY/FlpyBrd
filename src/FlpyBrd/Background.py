64c61a6ab5290889389dba039228285aadc18aca
JialaiY <142963141+JialaiY@users.noreply.github.com>
2024-05-16 23:25:02 -0600

    Delete src/FlpyBrd directory

--- src/FlpyBrd/Background.py
+++
@@ -1,9 +0,0 @@
-class Background:
-    import pygame
-    width = 500
-    height = 500
-    xpos = width / 2
-    ypos = height / 2
-    screen = pygame.display.set_mode((width,height))
-    loadStart = pygame.image.load("startscreen.png")
-    startScreenSize = pygame.transform.scale(loadStart, (width, height))
--- src/FlpyBrd/Bird.py
+++
@@ -1,9 +0,0 @@
-class Bird:
-    import pygame
-    width = 100
-    height = 100
-    xpos = width/2
-    ypos = height/2
-
-    loadBird = pygame.image.load("beebrd.png")
-    birdSize = pygame.transform.scale(loadBird, (width, height))
